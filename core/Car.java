5be0882c00ab37ca14214ea9691ef3dbf17b9cb2
vellprolol <vellprolol@gmail.com>
2020-10-08 11:58:50 +0300

    getters and setters were added

--- core/Car.java
+++ core/Car.java
@@ -7,6 +7,43 @@ public class Car {
     public boolean hasVehicle;
     public boolean isSpecial;
 
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setHasVehicle (boolean hasVehicle) {
+        this.hasVehicle = hasVehicle;
+    }
+    public boolean getHasVehucle() {
+        return hasVehicle;
+    }
+
+    public void isSpecial (boolean isSpecial) {
+        this.isSpecial = isSpecial;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+
     public String toString() {
         String special = isSpecial ? "СПЕЦТРАНСПОРТ " : "";
         return "\n=========================================\n" +
